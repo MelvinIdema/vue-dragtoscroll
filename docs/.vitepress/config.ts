69e6d41ff6a9ec75e30426edee2826c94dd6f3e5
melvinprindustry <144001132+melvinprindustry@users.noreply.github.com>
2024-11-12 19:41:08 +0100

    Add github action for deploying documentation

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/vue-dragtoscroll/',
   title: "VueDragToScroll",
   description: "A Vue composable for drag to scroll.",
   themeConfig: {
